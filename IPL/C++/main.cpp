f08ff588783587a2bfe2575d87a97d4e5c328591
Rushabh <20rush09abh2001@gmail.com>
2025-04-22 14:12:12 +0530

    Added C++ solution to 'IPL'

---
+++ IPL/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: IPL
+// Link to the problem: https://www.codechef.com/problems/INPL
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    if (t >= 7)
+    {
+        cout << "THALA" << endl;
+    }
+    else
+    {
+        cout << "BOOM" << endl;
+    }
+    return 0;
+}
