39813ce2248cfc14023803673ceb3e8d03317735
Rushabh <20rush09abh2001@gmail.com>
2025-04-24 16:16:50 +0530

    Added C++ solution to 'Max Sixers'

---
+++ Max Sixers/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Max Sixers
+// Link to the problem: https://www.codechef.com/problems/MAX6
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x;
+    cin >> x;
+    cout << x / 6 << endl;
+    return 0;
+}
