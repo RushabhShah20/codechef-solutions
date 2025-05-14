ef5cf4de0c329256a099a233cd37ffacbd55a11a
Rushabh <20rush09abh2001@gmail.com>
2025-05-14 14:56:36 +0530

    Added C++ solution to 'Cricket World Cup Qualifier'

---
+++ Cricket World Cup Qualifier/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Cricket World Cup Qualifier
+// Link to the problem: https://www.codechef.com/problems/CWC23QUALIF
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    if (n >= 12)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
