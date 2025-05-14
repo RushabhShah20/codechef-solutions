e23ef7f036dadbc9ddceb96270d6266cf0987af5
Rushabh <20rush09abh2001@gmail.com>
2025-05-15 01:08:52 +0530

    Added C++ solution to 'Chess Win'

---
+++ Chess Win/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Chess Win
+// Link to the problem: https://www.codechef.com/problems/CHESSWIN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << b - a + 1 << endl;
+    return 0;
+}
