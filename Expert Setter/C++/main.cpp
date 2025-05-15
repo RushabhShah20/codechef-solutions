1cbfe6054dea6478c461ec498eeed6af19b64d0d
Rushabh <20rush09abh2001@gmail.com>
2025-05-15 12:19:03 +0530

    Added C++ solution to 'Expert Setter'

---
+++ Expert Setter/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Expert Setter
+// Link to the problem: https://www.codechef.com/problems/EXPERT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        if (x <= 2 * y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
