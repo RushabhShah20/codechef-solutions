32ee8326952e526c17a786dca15037fee8b94d73
Rushabh <20rush09abh2001@gmail.com>
2025-05-15 12:38:49 +0530

    Added C++ solution to 'Clear Day'

---
+++ Clear Day/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Clear Day
+// Link to the problem: https://www.codechef.com/problems/CLEARDAY
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << 7 - (x + y) << endl;
+    return 0;
+}
