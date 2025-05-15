6631c13b648ad05c525e34c0f1ae578563fa1938
Rushabh <20rush09abh2001@gmail.com>
2025-05-15 12:38:34 +0530

    Added C++ solution to 'Lucky Seven'

---
+++ Lucky Seven/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Lucky Seven
+// Link to the problem: https://www.codechef.com/problems/LUCKYSEVEN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    string s;
+    cin >> s;
+    cout << s[6] << endl;
+    return 0;
+}
