5d542af087f163ea6800cca83c520f9271b50e21
Rushabh <20rush09abh2001@gmail.com>
2025-05-16 13:12:38 +0530

    Added C++ solution to 'Saving Taxes'

---
+++ Saving Taxes/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Saving Taxes
+// Link to the problem: https://www.codechef.com/problems/TAXSAVING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        cout << x - y << endl;
+    }
+    return 0;
+}
