b1c543d6e1984ccc3584f28b90675eef7815bc69
Rushabh <20rush09abh2001@gmail.com>
2025-05-16 13:11:56 +0530

    Added C++ solution to 'Count the Notebooks'

---
+++ Count the Notebooks/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Count the Notebooks
+// Link to the problem: https://www.codechef.com/problems/NOTEBOOK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << 10 * n << endl;
+    }
+    return 0;
+}
