449ce99ccff072ad3d22853dfd7e351f7fc2c91c
Rushabh <20rush09abh2001@gmail.com>
2025-05-16 13:13:22 +0530

    Added C++ solution to 'Sugarcane Juice Business'

---
+++ Sugarcane Juice Business/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Sugarcane Juice Business
+// Link to the problem: https://www.codechef.com/problems/SUGARCANE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << n * 50 * 30 / 100 << endl;
+    }
+    return 0;
+}
