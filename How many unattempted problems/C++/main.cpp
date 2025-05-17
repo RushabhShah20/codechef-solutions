166202269dd898bfb62d16b8f05324540592d0a0
Rushabh <20rush09abh2001@gmail.com>
2025-05-17 13:56:58 +0530

    Added C++ solution to 'How many unattempted problems'

---
+++ How many unattempted problems/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: How many unattempted problems
+// Link to the problem: https://www.codechef.com/problems/PRACLIST
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << x - y << endl;
+    return 0;
+}
