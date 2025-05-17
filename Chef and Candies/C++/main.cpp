f8fb9a41c56326d10320053d72b5a2993d97cdc5
Rushabh <20rush09abh2001@gmail.com>
2025-05-17 14:11:46 +0530

    Added C++ solution to 'Chef and Candies'

---
+++ Chef and Candies/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Chef and Candies
+// Link to the problem: https://www.codechef.com/problems/CHEFCAND
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, x;
+        cin >> n >> x;
+        if (n <= x)
+        {
+            cout << 0 << endl;
+        }
+        else
+        {
+            if ((n - x) % 4 == 0)
+            {
+                cout << ((n - x) / 4) << endl;
+            }
+            else
+            {
+                cout << ((n - x) / 4) + 1 << endl;
+            }
+        }
+    }
+    return 0;
+}
