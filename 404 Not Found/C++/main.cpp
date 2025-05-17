8e47379564afa36724dbff96cf22aeb2402e7ab7
Rushabh <20rush09abh2001@gmail.com>
2025-05-17 13:55:58 +0530

    Added C++ solution to '404 Not Found'

---
+++ 404 Not Found/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: 404 Not Found
+// Link to the problem: https://www.codechef.com/problems/ERROR404
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x;
+    cin >> x;
+    if (x == 404)
+    {
+        cout << "NOT FOUND" << endl;
+    }
+    else
+    {
+        cout << "FOUND" << endl;
+    }
+    return 0;
+}
