780d29ea3f5ff666e1e62a58e9db18f281922f14
Rushabh <20rush09abh2001@gmail.com>
2025-05-17 13:56:30 +0530

    Added C++ solution to 'Chef Plays Ludo'

---
+++ Chef Plays Ludo/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Chef Plays Ludo
+// Link to the problem: https://www.codechef.com/problems/LUDO
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        if (x == 6)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
