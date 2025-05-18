31be0e1f33299d2da3f609e2197d397be913a18c
Rushabh <20rush09abh2001@gmail.com>
2025-05-18 13:35:27 +0530

    Added C++ solution to 'Donation Drive'

---
+++ Donation Drive/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Donation Drive
+// Link to the problem: https://www.codechef.com/problems/DONDRIVE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, x;
+        cin >> n >> x;
+        cout << n - x << endl;
+    }
+    return 0;
+}
