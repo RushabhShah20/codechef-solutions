5805a9a3a7b70ba2f07af680a56be16bda2fbc04
Rushabh <20rush09abh2001@gmail.com>
2025-05-19 11:52:41 +0530

    Added C++ solution to 'Best of Two'

---
+++ Best of Two/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Best of Two
+// Link to the problem: https://www.codechef.com/problems/BESTOFTWO
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        if (x >= y)
+        {
+            cout << x << endl;
+        }
+        else
+        {
+            cout << y << endl;
+        }
+    }
+    return 0;
+}
