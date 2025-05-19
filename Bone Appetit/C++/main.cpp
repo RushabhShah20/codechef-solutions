7bb11974983581dde989bb45ba0b8cf651edf051
Rushabh <20rush09abh2001@gmail.com>
2025-05-19 11:53:01 +0530

    Added C++ solution to 'Bone Appetit'

---
+++ Bone Appetit/C++/main.cpp
@@ -0,0 +1,15 @@
+// Problem: Bone Appetit
+// Link to the problem: https://www.codechef.com/problems/BNE_APT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll n, m, x, y;
+    cin >> n >> m;
+    cin >> x >> y;
+    cout << (n * x) + (m * y) << endl;
+    return 0;
+}
