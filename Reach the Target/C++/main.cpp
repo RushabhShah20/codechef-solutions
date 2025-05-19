78b6a4dc368b030e8854d0df6c0cd1f95459837d
Rushabh <20rush09abh2001@gmail.com>
2025-05-19 11:53:46 +0530

    Added C++ solution to 'Reach the Target'

---
+++ Reach the Target/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Reach the Target
+// Link to the problem: https://www.codechef.com/problems/REACHTARGET
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        cout << x - y << endl;
+    }
+    return 0;
+}
