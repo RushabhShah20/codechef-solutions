88de5fde4ffb8a446ac32995db8983751a913e40
Rushabh <20rush09abh2001@gmail.com>
2025-05-20 13:14:17 +0530

    Added C++ solution to 'Parity'

---
+++ Parity/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Parity
+// Link to the problem: https://www.codechef.com/problems/PAR2
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n % 2 == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
