651e89a355637242c76259bc5a68dcc53a9864b4
Rushabh <20rush09abh2001@gmail.com>
2025-05-20 13:13:46 +0530

    Added C++ solution to 'Candy Division'

---
+++ Candy Division/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Candy Division
+// Link to the problem: https://www.codechef.com/problems/CANDIVIDE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        if (n % 3 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
