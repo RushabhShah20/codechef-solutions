5c1ab6ff21eb1a7b9f26cdd01c6c832fe2e277c1
Rushabh <20rush09abh2001@gmail.com>
2025-05-20 13:13:27 +0530

    Added C++ solution to '2000'

---
+++ 2000/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: 2000
+// Link to the problem: https://www.codechef.com/problems/RIP2000
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll n;
+    cin >> n;
+    cout << 4 * n << endl;
+    return 0;
+}
