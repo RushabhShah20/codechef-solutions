003380086b53c8d3fca2eecc27b93f41efd4b895
Rushabh <20rush09abh2001@gmail.com>
2025-05-21 10:50:19 +0530

    Added C++ solution to 'Ageing'

---
+++ Ageing/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Ageing
+// Link to the problem: https://www.codechef.com/problems/AGEING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        cout << x - 10 << endl;
+    }
+    return 0;
+}
