9538728a6cf8a264b531f4979f59757c0f40e4b5
Rushabh <20rush09abh2001@gmail.com>
2025-05-21 10:50:53 +0530

    Added C++ solution to 'Counting Words'

---
+++ Counting Words/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Counting Words
+// Link to the problem: https://www.codechef.com/problems/CNTWRD
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        cout << n * m << endl;
+    }
+    return 0;
+}
