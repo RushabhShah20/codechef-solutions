46145fed4a7cbc347cdade876b8dcd3f793b771c
Rushabh <20rush09abh2001@gmail.com>
2025-05-22 11:38:56 +0530

    Added C++ solution to 'Chef and Donation'

---
+++ Chef and Donation/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Chef and Donation
+// Link to the problem: https://www.codechef.com/problems/DNATION
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        cout << y - x << endl;
+    }
+    return 0;
+}
