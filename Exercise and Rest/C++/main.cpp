e616f6c72894ed7e43431855b39a7e6010f5f82f
Rushabh <20rush09abh2001@gmail.com>
2025-05-22 11:00:44 +0530

    Added C++ solution to 'Exercise and Rest'

---
+++ Exercise and Rest/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Exercise and Rest
+// Link to the problem: https://www.codechef.com/problems/EXREST
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    cout << 3 * n << endl;
+    return 0;
+}
