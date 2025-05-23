d2d3ffc226a7881fcd426b90656f6cc307bcdcc7
Rushabh <20rush09abh2001@gmail.com>
2025-05-23 12:31:36 +0530

    Added C++ solution to 'Couple Game'

---
+++ Couple Game/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Couple Game
+// Link to the problem: https://www.codechef.com/problems/COUGAME
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll g, b;
+        cin >> g >> b;
+        cout << b - g << endl;
+    }
+    return 0;
+}
