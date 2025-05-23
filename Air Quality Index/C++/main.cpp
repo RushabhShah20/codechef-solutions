265447e8fc30bffa67a872ac9633174e29ee3c98
Rushabh <20rush09abh2001@gmail.com>
2025-05-23 12:47:06 +0530

    Added C++ solution to 'Air Quality Index'

---
+++ Air Quality Index/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Air Quality Index
+// Link to the problem: https://www.codechef.com/problems/AIRINDEX
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x;
+    cin >> x;
+    if (x < 100)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
