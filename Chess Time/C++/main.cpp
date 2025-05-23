99a474519907b22012ff576133285fe6ecc1c5c8
Rushabh <20rush09abh2001@gmail.com>
2025-05-23 12:31:21 +0530

    Added C++ solution to 'Chess Time'

---
+++ Chess Time/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Chess Time
+// Link to the problem: https://www.codechef.com/problems/CHESSTIME
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << 3 * n << endl;
+    }
+    return 0;
+}
