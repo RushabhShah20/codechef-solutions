0d6b55a38c468c106ce308b0837f2fad14a8bad5
Rushabh <20rush09abh2001@gmail.com>
2025-05-24 12:35:11 +0530

    Added C++ solution to 'Water Requirement'

---
+++ Water Requirement/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Water Requirement
+// Link to the problem: https://www.codechef.com/problems/WATERREQ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << 2 * n << endl;
+    }
+    return 0;
+}
