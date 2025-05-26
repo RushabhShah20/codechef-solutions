bb973d84a8df3adda2fe5be3abb55c3e53d25136
Rushabh <20rush09abh2001@gmail.com>
2025-05-26 12:39:07 +0530

    Added C++ solution to 'True and False Paper'

---
+++ True and False Paper/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: True and False Paper
+// Link to the problem: https://www.codechef.com/problems/TFPAPER
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, k;
+        cin >> n >> k;
+        cout << n - k << endl;
+    }
+    return 0;
+}
