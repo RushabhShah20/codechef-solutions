923f646a716241d4c45a3a3bbe37fae1d1ed9498
Rushabh <20rush09abh2001@gmail.com>
2025-05-26 12:37:48 +0530

    Added C++ solution to 'Minimum Coins'

---
+++ Minimum Coins/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Minimum Coins
+// Link to the problem: https://www.codechef.com/problems/MINCOINSREQ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        cout << x % 10 << endl;
+    }
+    return 0;
+}
