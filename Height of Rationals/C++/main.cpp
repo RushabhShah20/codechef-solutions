ef864ae3851873cf1cc31d6dea4203d02a11fd65
Rushabh <20rush09abh2001@gmail.com>
2025-05-26 12:37:29 +0530

    Added C++ solution to 'Height of Rationals'

---
+++ Height of Rationals/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Height of Rationals
+// Link to the problem: https://www.codechef.com/problems/HEIGHTRATION
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << max(a, b) << endl;
+    return 0;
+}
