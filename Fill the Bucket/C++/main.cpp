b1f4ac10aa2916fb775e94bdc9bafacf06931bba
Rushabh <20rush09abh2001@gmail.com>
2025-05-27 14:07:28 +0530

    Added C++ solution to 'Fill the Bucket'

---
+++ Fill the Bucket/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Fill the Bucket
+// Link to the problem: https://www.codechef.com/problems/FBC
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll k, x;
+        cin >> k >> x;
+        cout << k - x << endl;
+    }
+    return 0;
+}
