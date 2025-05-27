4d71436a41cc10408f0d21440ad883658a72dd37
Rushabh <20rush09abh2001@gmail.com>
2025-05-27 14:07:42 +0530

    Added C++ solution to 'Find Remainder'

---
+++ Find Remainder/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Find Remainder
+// Link to the problem: https://www.codechef.com/problems/FLOW002
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll a, b;
+        cin >> a >> b;
+        cout << a % b << endl;
+    }
+    return 0;
+}
