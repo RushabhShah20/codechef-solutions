7783de808c1e29faaecf38ae3f7d7d6ef7e0dcc9
Rushabh <20rush09abh2001@gmail.com>
2025-05-27 14:07:55 +0530

    Added C++ solution to 'Instagram'

---
+++ Instagram/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Instagram
+// Link to the problem: https://www.codechef.com/problems/INSTAGRAM
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        if (x > 10 * y)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
