5abf211e87c2c6264a8e2d9be1b40ebd818638f5
Rushabh <20rush09abh2001@gmail.com>
2025-05-28 12:46:34 +0530

    Added C++ solution to 'Maximum Submissions'

---
+++ Maximum Submissions/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Maximum Submissions
+// Link to the problem: https://www.codechef.com/problems/MAXIMUMSUBS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x;
+        cin >> x;
+        cout << 2 * x << endl;
+    }
+    return 0;
+}
