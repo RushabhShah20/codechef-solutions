f37a678bbbf89443d35605f9bd335c6c93a40bdf
Rushabh <20rush09abh2001@gmail.com>
2025-05-29 11:10:01 +0530

    Added C++ solution to 'Increase IQ'

---
+++ Increase IQ/C++/main.cpp
@@ -0,0 +1,20 @@
+// Problem: Increase IQ
+// Link to the problem: https://www.codechef.com/problems/INCRIQ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+int main()
+{
+    ll x;
+    cin >> x;
+    if (x + 7 > 170)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
