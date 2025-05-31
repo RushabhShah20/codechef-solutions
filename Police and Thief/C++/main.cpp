77c3d07a91460959a7f8f854b6b6aab4d15dc6fb
Rushabh <20rush09abh2001@gmail.com>
2025-05-31 16:46:33 +0530

    Added C++ solution to 'Police and Thief'

---
+++ Police and Thief/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Police and Thief
+// Link to the problem: https://www.codechef.com/problems/POLTHIEF
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll x, y;
+        cin >> x >> y;
+        cout << abs(x - y) << endl;
+    }
+    return 0;
+}
