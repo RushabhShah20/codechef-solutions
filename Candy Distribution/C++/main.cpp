89943c854de22ab6d8b40d0831d0770ab425c950
Rushabh <20rush09abh2001@gmail.com>
2025-06-01 09:11:42 +0530

    Added C++ solution to 'Candy Distribution'

---
+++ Candy Distribution/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Candy Distribution
+// Link to the problem: https://www.codechef.com/problems/CANDYDIST
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, m;
+        cin >> n >> m;
+        if (n % m == 0 && (n / m) % 2 == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+    }
+    return 0;
+}
