96ba97bb8e37ab4d4f52aaeb912bf2d07de57298
Rushabh <20rush09abh2001@gmail.com>
2025-06-05 12:51:49 +0530

    Added C++ solution to 'Jenga Night'

---
+++ Jenga Night/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Jenga Night
+// Link to the problem: https://www.codechef.com/problems/JENGA
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n, x;
+        cin >> n >> x;
+        if (x % n == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
