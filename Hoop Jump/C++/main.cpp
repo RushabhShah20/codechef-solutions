082993c6ae4082312988caf7e07dd59bdf646369
Rushabh <20rush09abh2001@gmail.com>
2025-06-06 13:47:26 +0530

    Added C++ solution to 'Hoop Jump'

---
+++ Hoop Jump/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: Hoop Jump
+// Link to the problem: https://www.codechef.com/problems/HOOPS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        ll n;
+        cin >> n;
+        cout << ((n / 2) + 1) << endl;
+    }
+    return 0;
+}
