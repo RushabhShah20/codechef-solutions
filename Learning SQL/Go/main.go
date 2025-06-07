01ac549ddddb8b25aa20e5f919c7f1f7b17ff212
Rushabh <20rush09abh2001@gmail.com>
2025-06-07 19:24:15 +0530

    Added Go solution to 'Learning SQL'

---
+++ Learning SQL/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: Learning SQL
+// Link to the problem: https://www.codechef.com/problems/LEARNSQL
+package main
+
+import "fmt"
+
+func main() {
+	var r, c, e int
+	fmt.Scan(&r, &c, &e)
+	fmt.Println((r + e) * c)
+}
