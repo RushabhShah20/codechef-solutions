e24874aa8831862e8766a12456fe719cbf09e987
Rushabh <20rush09abh2001@gmail.com>
2025-06-07 19:23:46 +0530

    Added Go solution to 'Chef and Instant Noodles'

---
+++ Chef and Instant Noodles/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: Chef and Instant Noodles
+// Link to the problem: https://www.codechef.com/problems/INSTNOODLE
+package main
+
+import "fmt"
+
+func main() {
+	var x, y int
+	fmt.Scan(&x, &y)
+	fmt.Println(x * y)
+}
