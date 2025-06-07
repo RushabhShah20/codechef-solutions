52deb033f675f43f016b935cbcbcbb47c9b30140
Rushabh <20rush09abh2001@gmail.com>
2025-06-07 19:24:33 +0530

    Added Go solution to 'Number Mirror'

---
+++ Number Mirror/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: Number Mirror
+// Link to the problem: https://www.codechef.com/problems/START01
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(n)
+}
