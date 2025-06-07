43a9a9d33e99b5e275b5b42122210dacbcb75c16
Rushabh <20rush09abh2001@gmail.com>
2025-06-07 19:24:01 +0530

    Added Go solution to 'CodeChef Learn Problem Solving'

---
+++ CodeChef Learn Problem Solving/Go/main.go
@@ -0,0 +1,11 @@
+// Problem: CodeChef Learn Problem Solving
+// Link to the problem: https://www.codechef.com/problems/CCLEARN
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+	fmt.Scan(&n)
+	fmt.Println(2 * n)
+}
