3c811be11766afb2c80f640f0294c485fcf9119d
Rushabh <20rush09abh2001@gmail.com>
2025-06-09 14:30:21 +0530

    Added C# solution to 'Number Mirror'

---
+++ Number Mirror/C#/main.cs
@@ -0,0 +1,12 @@
+// Problem: Number Mirror
+// Link to the problem: https://www.codechef.com/problems/START01
+using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        int number = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine(number);
+    }
+}
