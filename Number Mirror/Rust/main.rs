8f81ae3dbd2807361894853b011222c5f905d1a4
Rushabh <20rush09abh2001@gmail.com>
2025-06-10 13:30:08 +0530

    Added Rust solution to 'Number Mirror'

---
+++ Number Mirror/Rust/main.rs
@@ -0,0 +1,7 @@
+// Problem: Number Mirror
+// Link to the problem: https://www.codechef.com/problems/START01
+fn main() {
+    let mut line = String::new();
+    _ = std::io::stdin().read_line(&mut line).unwrap();
+    println!("{}", line);
+}
