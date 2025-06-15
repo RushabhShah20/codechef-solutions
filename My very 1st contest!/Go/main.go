d81e0a6d06fad40005f129b8312ea0831e0b8011
Rushabh <20rush09abh2001@gmail.com>
2025-06-15 15:43:34 +0530

    Added Go solution to 'My very 1st contest!'

---
+++ My very 1st contest!/Go/main.go
@@ -0,0 +1,13 @@
+// Problem: My very 1st contest!
+// Link to the problem: https://www.codechef.com/problems/MY1STCONTEST
+package main
+
+import "fmt"
+
+func main() {
+	var n, a, b int
+
+	fmt.Scanf("%d %d %d", &n, &a, &b)
+
+	fmt.Println(n-a, n-a-b)
+}
