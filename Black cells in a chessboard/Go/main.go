5e451001c4928e8ab7c14bd3bd174571f8dc8a3e
Rushabh <20rush09abh2001@gmail.com>
2025-06-15 15:42:48 +0530

    Added Go solution to 'Black cells in a chessboard'

---
+++ Black cells in a chessboard/Go/main.go
@@ -0,0 +1,13 @@
+// Problem: Black cells in a chessboard
+// Link to the problem: https://www.codechef.com/problems/BLACKCEL
+package main
+
+import "fmt"
+
+func main() {
+	var n int
+
+	fmt.Scanf("%d", &n)
+
+	fmt.Println((n * n) / 2)
+}
