c9bb128ac3c5aca239a4ffd6c6072868b4ff5c1a
Rushabh <20rush09abh2001@gmail.com>
2025-06-24 12:18:16 +0530

    Added Rust solution to 'Squats'

---
+++ Solutions/Squats/Rust/main.rs
@@ -0,0 +1,23 @@
+// Problem: Squats
+// Link to the problem: https://www.codechef.com/problems/SQUATS
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let t: i32 = input.trim().parse().expect("Failed to parse integer");
+
+    for _ in 0..t {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let x: i32 = input.trim().parse().expect("Failed to parse integer");
+
+        println!("{}", (x * 15));
+    }
+}
