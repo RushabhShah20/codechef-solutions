9799a1285573e0f372f6f62b8e514910b1418468
Rushabh <20rush09abh2001@gmail.com>
2025-06-24 12:18:01 +0530

    Added Rust solution to 'Oneful Pairs'

---
+++ Solutions/Oneful Pairs/Rust/main.rs
@@ -0,0 +1,28 @@
+// Problem: Oneful Pairs
+// Link to the problem: https://www.codechef.com/problems/ONEFULPAIRS
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let mut iter = input.trim().split_whitespace();
+    let a: i32 = iter
+        .next()
+        .expect("No value found")
+        .parse()
+        .expect("Failed to parse integer");
+    let b: i32 = iter
+        .next()
+        .expect("No value found")
+        .parse()
+        .expect("Failed to parse integer");
+
+    if (a + b + (a * b) == 111) {
+        println!("Yes")
+    } else {
+        println!("No")
+    }
+}
