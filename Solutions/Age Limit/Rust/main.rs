d6e3729ee636e1c834237c1fff1350fd04ad5905
Rushabh <20rush09abh2001@gmail.com>
2025-06-24 12:16:46 +0530

    Added Rust solution to 'Age Limit'

---
+++ Solutions/Age Limit/Rust/main.rs
@@ -0,0 +1,42 @@
+// Problem: Age Limit
+// Link to the problem: https://www.codechef.com/problems/AGELIMIT
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin()
+        .read_line(&mut input)
+        .expect("Failed to read line");
+
+    let t: i32 = input.trim().parse().expect("Failed to parse integer");
+
+    for _ in 0..t {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("Failed to read line");
+
+        let mut iter = input.trim().split_whitespace();
+        let x: i32 = iter
+            .next()
+            .expect("No value found")
+            .parse()
+            .expect("Failed to parse integer");
+        let y: i32 = iter
+            .next()
+            .expect("No value found")
+            .parse()
+            .expect("Failed to parse integer");
+        let a: i32 = iter
+            .next()
+            .expect("No value found")
+            .parse()
+            .expect("Failed to parse integer");
+
+        if (a >= x && a < y) {
+            println!("YES");
+        } else {
+            println!("NO");
+        }
+    }
+}
