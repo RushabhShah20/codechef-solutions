1c38c1fe97ce440ae0533c76fe198d42bada1c6c
Rushabh <20rush09abh2001@gmail.com>
2025-07-17 17:56:54 +0530

    Added C++ solution to 'Chef Bakes Cake 1'

---
+++ Solutions/Chef Bakes Cake 1/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Chef Bakes Cake 1
+// Link to the problem: https://www.codechef.com/problems/BAKECAKE1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll m, n;
+    cin >> n >> m;
+    cout << (m * 50) - (n * 30) << endl;
+    return 0;
+}
