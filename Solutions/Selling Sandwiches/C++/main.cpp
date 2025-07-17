ac4e6e2cca02cc11280091d97f5b826669abb910
Rushabh <20rush09abh2001@gmail.com>
2025-07-17 17:53:33 +0530

    Added C++ solution to 'Selling Sandwiches'

---
+++ Solutions/Selling Sandwiches/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Selling Sandwiches
+// Link to the problem: https://www.codechef.com/problems/SANDWSHOP
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    cout << a - (b + c) << endl;
+    return 0;
+}
