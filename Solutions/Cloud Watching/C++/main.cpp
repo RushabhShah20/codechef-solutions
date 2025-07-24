013ac318c7ea96f28fbc18564eb2e2ed39dfff85
Rushabh <20rush09abh2001@gmail.com>
2025-07-24 10:53:36 +0530

    Added C++ solution to 'Cloud Watching'

---
+++ Solutions/Cloud Watching/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Cloud Watching
+// Link to the problem: https://www.codechef.com/problems/MORECOOK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (b >= 3 * a)
+    {
+        cout << "Rain" << endl;
+    }
+    else
+    {
+        cout << "Dry" << endl;
+    }
+
+    return 0;
+}
