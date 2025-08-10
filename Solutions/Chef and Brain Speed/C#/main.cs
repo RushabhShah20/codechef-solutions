480266632eca7ae6b4ee0136c8028540b6909218
Rushabh <20rush09abh2001@gmail.com>
2025-08-10 12:13:13 +0530

    Added C# solution to 'Chef and Brain Speed'

---
+++ Solutions/Chef and Brain Speed/C#/main.cs
@@ -0,0 +1,20 @@
+// Problem: Chef and Brain Speed
+// Link to the problem: https://www.codechef.com/problems/CBSPEED
+using System;
+public class Test
+{
+    public static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        int x = int.Parse(s[0]);
+        int y = int.Parse(s[1]);
+        if (y > x)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
