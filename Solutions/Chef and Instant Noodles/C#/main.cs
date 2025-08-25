26d0c018ce223bab82db1c74d6c34079bc963176
Rushabh <20rush09abh2001@gmail.com>
2025-08-25 17:33:55 +0530

    Added C# solution to 'Chef and Instant Noodles'

---
+++ Solutions/Chef and Instant Noodles/C#/main.cs
@@ -0,0 +1,14 @@
+// Problem: Chef and Instant Noodles
+// Link to the problem: https://www.codechef.com/problems/INSTNOODLE
+using System;
+
+public class Test
+{
+    public static void Main()
+    {
+        string[] s = Console.ReadLine().Split(' ');
+        int x = int.Parse(s[0]);
+        int y = int.Parse(s[1]);
+        Console.WriteLine(x * y);
+    }
+}
