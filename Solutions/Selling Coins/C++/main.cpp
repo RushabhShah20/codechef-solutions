750c14c257d2228791d959c8604fb9d319bec653
Rushabh <20rush09abh2001@gmail.com>
2025-08-28 11:43:25 +0530

    Added C++ solution to 'Selling Coins'

---
+++ Solutions/Selling Coins/C++/main.cpp
@@ -0,0 +1,15 @@
+// Problem: Selling Coins
+// Link to the problem: https://www.codechef.com/problems/SELLCOIN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+
+    cout << a + (2 * b) << endl;
+    return 0;
+}
