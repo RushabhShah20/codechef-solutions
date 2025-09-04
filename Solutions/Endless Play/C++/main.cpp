785ead6a1d7b5dbd0ab9191e02bde8274b1c20b2
Rushabh <20rush09abh2001@gmail.com>
2025-09-04 14:23:34 +0530

    Added C++ solution to 'Endless Play'

---
+++ Solutions/Endless Play/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Endless Play
+// Link to the problem: https://www.codechef.com/problems/SKONG
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x, h;
+    cin >> x >> h;
+    cout << ((x - 4) * 24) + h << endl;
+    return 0;
+}
