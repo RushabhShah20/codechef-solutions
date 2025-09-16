d57410a8a4f010b2eec286e4be752b38c18bd7c6
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 18:06:41 +0530

    Added Java solution to 'Chef and Brain Speed'

---
+++ Solutions/Chef and Brain Speed/Java/main.java
@@ -0,0 +1,19 @@
+
+// Problem: Chef and Brain Speed
+// Link to the problem: https://www.codechef.com/problems/CBSPEED
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (y > x) {
+            System.out.println("YES");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
