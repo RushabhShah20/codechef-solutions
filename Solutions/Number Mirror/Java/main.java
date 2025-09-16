6e206ea18caa70f7336cbca427628da7c4bc07de
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 17:51:54 +0530

    Added Java solution to 'Number Mirror'

---
+++ Solutions/Number Mirror/Java/main.java
@@ -0,0 +1,13 @@
+
+// Problem: Number Mirror
+// Link to the problem: https://www.codechef.com/problems/START01
+import java.util.Scanner;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        System.out.println(n);
+        sc.close();
+    }
+}
