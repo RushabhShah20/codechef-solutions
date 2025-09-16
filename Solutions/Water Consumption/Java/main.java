1d9a9eeab7c4b876fd02a65a56eb0decbd39f8ce
Rushabh <20rush09abh2001@gmail.com>
2025-09-17 04:18:01 +0530

    Added Java solution to 'Water Consumption'

---
+++ Solutions/Water Consumption/Java/main.java
@@ -0,0 +1,21 @@
+
+// Problem: Water Consumption
+// Link to the problem: https://www.codechef.com/problems/WATERCONS
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            if (x >= 2000) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
