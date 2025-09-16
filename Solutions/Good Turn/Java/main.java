296c7d4c9102c960380f8f6ba8af3453ebf0e1f7
Rushabh <20rush09abh2001@gmail.com>
2025-09-17 04:21:53 +0530

    Added Java solution to 'Good Turn'

---
+++ Solutions/Good Turn/Java/main.java
@@ -0,0 +1,22 @@
+
+// Problem: Good Turn
+// Link to the problem: https://www.codechef.com/problems/GDTURN
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int x = sc.nextInt();
+            int y = sc.nextInt();
+            if (x + y > 6) {
+                System.out.println("YES");
+            } else {
+                System.out.println("NO");
+            }
+        }
+    }
+}
