00f014ee6c51c323d5e08642b562ff58084409ec
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 17:54:44 +0530

    Added Java solution to 'Chef and Instant Noodles'

---
+++ Solutions/Chef and Instant Noodles/Java/main.java
@@ -0,0 +1,17 @@
+
+// Problem: Chef and Instant Noodles
+// Link to the problem: https://www.codechef.com/problems/INSTNOODLE
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int x, y;
+        x = in.nextInt();
+        y = in.nextInt();
+        System.out.println(x * y);
+        in.close();
+    }
+}
