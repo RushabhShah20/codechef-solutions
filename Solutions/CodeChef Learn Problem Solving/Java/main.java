4852de84899d9d42eecd54591000b72805812600
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 17:57:15 +0530

    Added Java solution to 'CodeChef Learn Problem Solving'

---
+++ Solutions/CodeChef Learn Problem Solving/Java/main.java
@@ -0,0 +1,16 @@
+
+// Problem: CodeChef Learn Problem Solving
+// Link to the problem: https://www.codechef.com/problems/CCLEARN
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+        System.out.println(2 * n);
+        in.close();
+    }
+}
