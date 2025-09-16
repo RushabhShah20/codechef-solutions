5f280875342b8878f90ba33b532df003a90c9eec
Rushabh <20rush09abh2001@gmail.com>
2025-09-17 04:19:43 +0530

    Added Java solution to 'Add Two Numbers'

---
+++ Solutions/Add Two Numbers/Java/main.java
@@ -0,0 +1,18 @@
+
+// Problem: Add Two Numbers
+// Link to the problem: https://www.codechef.com/problems/FLOW001
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = sc.nextInt();
+        while (t-- > 0) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            System.out.println(a + b);
+        }
+    }
+}
