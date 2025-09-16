da40782aae0c9014c9740ba170e982da8a899f87
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 18:10:25 +0530

    Added Java solution to 'Oneful Pairs'

---
+++ Solutions/Oneful Pairs/Java/main.java
@@ -0,0 +1,19 @@
+
+// Problem: Oneful Pairs
+// Link to the problem: https://www.codechef.com/problems/ONEFULPAIRS
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if (a + b + (a * b) == 111) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
