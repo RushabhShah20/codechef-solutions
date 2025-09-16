b205244866eff70c1cb1681c0a37c6ab208c50d8
Rushabh <20rush09abh2001@gmail.com>
2025-09-16 18:08:41 +0530

    Added Java solution to 'Alice and Marks'

---
+++ Solutions/Alice and Marks/Java/main.java
@@ -0,0 +1,19 @@
+
+// Problem: Alice and Marks
+// Link to the problem: https://www.codechef.com/problems/MARKSTW
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        if (x >= 2 * y) {
+            System.out.println("Yes");
+        } else {
+            System.out.println("No");
+        }
+    }
+}
