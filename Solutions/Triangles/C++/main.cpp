d7a20290856e57e6b5fce39c068dcdcc5c0ec694
Rushabh <20rush09abh2001@gmail.com>
2025-09-18 00:31:58 +0530

    Added C++ solution to 'Triangles'

---
+++ Solutions/Triangles/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Triangles
+// Link to the problem: https://www.codechef.com/problems/TRIANGLE7
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << 180 - (a + b) << endl;
+    return 0;
+}
