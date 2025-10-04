cefa65627da14bff3cc7eee2b9467f03a81638c1
Rushabh <20rush09abh2001@gmail.com>
2025-10-04 16:28:40 +0530

    Added C++ solution to 'Reach 5 Star'

---
+++ Solutions/Reach 5 Star/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Reach 5 Star
+// Link to the problem: https://www.codechef.com/problems/R5S
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x, y;
+    cin >> x >> y;
+    if (x + y >= 2000)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
