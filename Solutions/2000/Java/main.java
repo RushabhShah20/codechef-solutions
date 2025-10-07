fe60855b0ef26e51856fff1a36ace056ab318b6f
Rushabh <20rush09abh2001@gmail.com>
2025-10-07 23:28:39 +0530

    Added Java solution to '2000'

---
+++ Solutions/2000/Java/main.java
@@ -0,0 +1,16 @@
+
+// Problem: 2000
+// Link to the problem: https://www.codechef.com/problems/RIP2000
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef {
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(System.in);
+        int n;
+        n = in.nextInt();
+        System.out.println(4 * n);
+        in.close();
+    }
+}
