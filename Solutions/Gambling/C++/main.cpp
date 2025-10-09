59c1c660cc2267af9a82de5a6cdfe9c5216aebd9
Rushabh <20rush09abh2001@gmail.com>
2025-10-09 14:11:24 +0530

    Added C++ solution to 'Gambling'

---
+++ Solutions/Gambling/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Gambling
+// Link to the problem: https://www.codechef.com/problems/GAMBLING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll a;
+    cin >> a;
+    cout << 7 - a << endl;
+    return 0;
+}
