086603de4256feee41854cec505c0f8e335d75cc
Rushabh <20rush09abh2001@gmail.com>
2025-10-16 14:11:37 +0530

    Added C++ solution to 'Spring Cleaning'

---
+++ Solutions/Spring Cleaning/C++/main.cpp
@@ -0,0 +1,14 @@
+// Problem: Spring Cleaning
+// Link to the problem: https://www.codechef.com/problems/SPRCLN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+int main()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << (30 * x) + (60 * y) << endl;
+    return 0;
+}
