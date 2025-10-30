f22c42af281bee08fb1b32e09f571a3d314b2eb8
Rushabh <20rush09abh2001@gmail.com>
2025-10-30 15:31:34 +0530

    Added C++ solution to 'Profits'

---
+++ Solutions/Profits/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Profits
+// Link to the problem: https://www.codechef.com/problems/PROFIT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    ll ans = 0;
+    for (ll i = x; i <= n; i++)
+    {
+        ans += (i - x);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
