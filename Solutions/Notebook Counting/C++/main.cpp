a4e4f6d57c4988655fd117ccf86a1548dc4376cd
Rushabh <20rush09abh2001@gmail.com>
2025-10-30 15:29:09 +0530

    Added C++ solution to 'Notebook Counting'

---
+++ Solutions/Notebook Counting/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Notebook Counting
+// Link to the problem: https://www.codechef.com/problems/NOTECNT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    cout << (100 * a * b) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
