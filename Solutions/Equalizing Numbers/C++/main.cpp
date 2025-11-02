a218268a45e9508190ba449b08e6c5dbe1f207f6
Rushabh <20rush09abh2001@gmail.com>
2025-11-02 15:49:57 +0530

    Added C++ solution to 'Equalizing Numbers'

---
+++ Solutions/Equalizing Numbers/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Equalizing Numbers
+// Link to the problem: https://www.codechef.com/problems/EQLZING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (abs(a - b) % 2 == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
