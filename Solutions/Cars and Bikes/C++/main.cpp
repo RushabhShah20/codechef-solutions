0f3fa20a64103db4ca569363a8bb8a938976c44a
Rushabh <20rush09abh2001@gmail.com>
2025-11-02 15:43:50 +0530

    Added C++ solution to 'Cars and Bikes'

---
+++ Solutions/Cars and Bikes/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Cars and Bikes
+// Link to the problem: https://www.codechef.com/problems/TYRES
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 4 == 0)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
