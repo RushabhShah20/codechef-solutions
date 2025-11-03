a0467b67cb6b743df1a6e539f2093c4fd24ecd84
Rushabh <20rush09abh2001@gmail.com>
2025-11-03 11:24:58 +0530

    Added C# solution to 'Black cells in a chessboard'

---
+++ Solutions/Black cells in a chessboard/C#/main.cs
@@ -0,0 +1,12 @@
+// Problem: Black cells in a chessboard
+// Link to the problem: https://www.codechef.com/problems/BLACKCEL
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        Console.WriteLine((n * n) / 2);
+    }
+}
