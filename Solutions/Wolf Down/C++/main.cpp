10cb58817a27ff252004b255a40d2f85cc8d6afb
Rushabh <20rush09abh2001@gmail.com>
2025-11-06 13:56:28 +0530

    Added C++ solution to 'Wolf Down'

---
+++ Solutions/Wolf Down/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Wolf Down
+// Link to the problem: https://www.codechef.com/problems/WOLFDOWN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll ans = 0;
+    for (char c : s)
+    {
+        if (c == '1')
+        {
+            break;
+        }
+        else
+        {
+            ans++;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
