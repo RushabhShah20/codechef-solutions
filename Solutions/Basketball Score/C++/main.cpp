b462b43e24b7d20e7cdfa69b0e485574c8cf5361
Rushabh <20rush09abh2001@gmail.com>
2025-11-13 14:33:53 +0530

    Added C++ solution to 'Basketball Score'

---
+++ Solutions/Basketball Score/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Basketball Score
+// Link to the problem: https://www.codechef.com/problems/P1BAR
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << ((3 * x) + (2 * y)) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
