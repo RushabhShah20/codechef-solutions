63f6830590b52fd29866d6c4f615b6c9d8861026
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 17:44:53 +0530

    Added C++ solution to 'Integers that sum to the same value'

---
+++ Solutions/Integers that sum to the same value/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: Integers that sum to the same value
+// Link to the problem: https://www.codechef.com/problems/SUMNEQ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << n - 1 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
