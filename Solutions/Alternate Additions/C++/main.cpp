e371a1b4e61034a41d0bb64c0836dcde2cf39d53
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 18:03:34 +0530

    Added C++ solution to 'Alternate Additions'

---
+++ Solutions/Alternate Additions/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Alternate Additions
+// Link to the problem: https://www.codechef.com/problems/ALTERADD
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if ((b - a) % 3 <= 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
