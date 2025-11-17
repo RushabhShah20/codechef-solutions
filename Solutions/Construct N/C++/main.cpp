2c061f469e8a12e79c3b0a6a1a1f1e807e2af7ea
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 17:55:02 +0530

    Added C++ solution to 'Construct N'

---
+++ Solutions/Construct N/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Construct N
+// Link to the problem: https://www.codechef.com/problems/CONN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n < 7)
+    {
+        if (n % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
