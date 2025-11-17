cfe91ed0a39261b800da9a4a07ea7501795d9208
Rushabh <20rush09abh2001@gmail.com>
2025-11-17 17:47:21 +0530

    Added C++ solution to 'Minimum Number of Ones'

---
+++ Solutions/Minimum Number of Ones/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Minimum Number of Ones
+// Link to the problem: https://www.codechef.com/problems/MINNOO
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (n / 2) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
