ce9da13ee102c98932be76960bbf20124188d0d0
Rushabh <20rush09abh2001@gmail.com>
2025-11-18 12:56:42 +0530

    Added C++ solution to 'Best of N Sets'

---
+++ Solutions/Best of N Sets/C++/main..cpp
@@ -0,0 +1,26 @@
+// Problem: Best of N Sets
+// Link to the problem: https://www.codechef.com/problems/BESTOFTENNIS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << ((max(x, y) * 2) - 1) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
