8f8959f79eb37914901616f66ab84fc196d1de21
Rushabh <20rush09abh2001@gmail.com>
2025-11-18 15:24:21 +0530

    Added C++ solution to 'Group Assignment'

---
+++ Solutions/Group Assignment/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Group Assignment
+// Link to the problem: https://www.codechef.com/problems/GROUPASSGN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    cout << (2 * n) + 1 - x << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
