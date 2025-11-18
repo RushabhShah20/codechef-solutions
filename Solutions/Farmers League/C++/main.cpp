83ef55da47ee94104a86f8f4cdc2d34fda9aa76a
Rushabh <20rush09abh2001@gmail.com>
2025-11-18 15:49:43 +0530

    Added C++ solution to 'Farmers League'

---
+++ Solutions/Farmers League/C++/main.cpp
@@ -0,0 +1,27 @@
+// Problem: Farmers League
+// Link to the problem: https://www.codechef.com/problems/LEAGUE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll ans = ((n - 1) * 3 - ceil((double)(n - 2) / 2) * 3);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
