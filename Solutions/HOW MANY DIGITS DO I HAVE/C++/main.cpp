8978e8f330bfe7bc7db3834b4522278441f15465
Rushabh <20rush09abh2001@gmail.com>
2025-11-19 11:17:00 +0530

    Added C++ solution to 'HOW MANY DIGITS DO I HAVE'

---
+++ Solutions/HOW MANY DIGITS DO I HAVE/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: HOW MANY DIGITS DO I HAVE
+// Link to the problem: https://www.codechef.com/problems/HOWMANY
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.size() == 1)
+    {
+        cout << "1" << endl;
+    }
+    else if (s.size() == 2)
+    {
+        cout << "2" << endl;
+    }
+    else if (s.size() == 3)
+    {
+        cout << "3" << endl;
+    }
+    else
+    {
+        cout << "More than 3 digits" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+
+    return 0;
+}
