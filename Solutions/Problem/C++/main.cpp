0d963eee8ef65989d78dc32bb7e706270cf67b92
Rushabh <20rush09abh2001@gmail.com>
2025-11-19 14:36:20 +0530

    Added C++ solution to 'Problem'

---
+++ Solutions/Problem/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Problem
+// Link to the problem: https://www.codechef.com/problems/PR0BLEM
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    if ((n - m) % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
