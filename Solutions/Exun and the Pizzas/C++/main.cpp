a51a52385ad197d603bf5a7ccbf5b7c5f25f5953
Rushabh <20rush09abh2001@gmail.com>
2025-11-20 11:20:51 +0530

    Added C++ solution to 'Exun and the Pizzas'

---
+++ Solutions/Exun and the Pizzas/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Exun and the Pizzas
+// Link to the problem: https://www.codechef.com/problems/EXUNPIZZA
+#include <bits/stdc++.h>
+#define ll long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k, r;
+    cin >> n >> k >> r;
+    cout << ((n - k) * r) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+
+    return 0;
+}
