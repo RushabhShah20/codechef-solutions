3a428e5234318c449851837b6e9557f7b9ec17c7
Rushabh <20rush09abh2001@gmail.com>
2025-11-20 14:06:25 +0530

    Added C++ solution to 'Remove Duplicates'

---
+++ Solutions/Remove Duplicates/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Remove Duplicates
+// Link to the problem: https://www.codechef.com/problems/PREP69
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    set<ll> s;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+    cout << s.size() << endl;
+    for (int i : s)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
