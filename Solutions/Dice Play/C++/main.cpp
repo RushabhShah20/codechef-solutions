a57ccaab57ebbcb9a258180533803d4cfc62148f
Rushabh <20rush09abh2001@gmail.com>
2025-11-27 13:43:42 +0530

    Added C++ solution to 'Dice Play'

---
+++ Solutions/Dice Play/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Dice Play
+// Link to the problem: https://www.codechef.com/problems/P1HOME
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (a == b)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
