3c1d6f1d22fe03cb3640bee58eeeb097d0499c09
Rushabh <20rush09abh2001@gmail.com>
2025-12-06 17:28:39 +0530

    Added C++ solution to 'Walk on the Axis'

---
+++ Solutions/Walk on the Axis/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Walk on the Axis
+// Link to the problem: https://www.codechef.com/problems/ANUWTA
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << (((n + 1) * (n + 2)) / 2) - 1 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
