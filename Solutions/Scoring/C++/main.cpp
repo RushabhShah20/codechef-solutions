6d74da0a1152b571be46010f8f838518027e2d50
Rushabh <20rush09abh2001@gmail.com>
2025-12-11 15:45:49 +0530

    Added C++ solution to 'Scoring'

---
+++ Solutions/Scoring/C++/main.cpp
@@ -0,0 +1,26 @@
+// Problem: Scoring
+// Link to the problem: https://www.codechef.com/problems/SCORING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << ((y + x) / 2) << " " << ((y - x) / 2) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
