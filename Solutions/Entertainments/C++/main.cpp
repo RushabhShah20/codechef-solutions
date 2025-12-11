9e5f97c6c30ebe929795bc2d955397cb72811b62
Rushabh <20rush09abh2001@gmail.com>
2025-12-11 15:44:49 +0530

    Added C++ solution to 'Entertainments'

---
+++ Solutions/Entertainments/C++/main.cpp
@@ -0,0 +1,23 @@
+// Problem: Entertainments
+// Link to the problem: https://www.codechef.com/problems/ENTERTAIN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << min(1000LL, 200 * n) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+
+    solve();
+
+    return 0;
+}
