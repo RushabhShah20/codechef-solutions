ef1a9e5e70d4988085e684d19bcd5936c0f31bba
Rushabh <20rush09abh2001@gmail.com>
2025-12-21 19:05:48 +0530

    Added C++ solution to 'Passing Marks'

---
+++ Solutions/Passing Marks - 2/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Passing Marks
+// Link to the problem: https://www.codechef.com/problems/CUTOFF
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = a[n - x] - 1;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
