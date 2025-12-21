d82ec628f25821ca6160a7a8c7fb702d9aee68e5
Rushabh <20rush09abh2001@gmail.com>
2025-12-21 19:01:24 +0530

    Added C++ solution to 'Simple Sorting'

---
+++ Solutions/Simple Sorting/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Simple Sorting
+// Link to the problem: https://www.codechef.com/problems/TSORT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
