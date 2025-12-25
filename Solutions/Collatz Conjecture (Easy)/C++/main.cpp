0f2c895729f1568dada6f0e08ffbc905790a036f
Rushabh <20rush09abh2001@gmail.com>
2025-12-25 16:35:16 +0530

    Added C++ solution to 'Collatz Conjecture (Easy)'

---
+++ Solutions/Collatz Conjecture (Easy)/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Collatz Conjecture (Easy)
+// Link to the problem: https://www.codechef.com/problems/COLLATZEZ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 4 == 0 || n == 1 || n == 2 || n == 3 || n == 6)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
