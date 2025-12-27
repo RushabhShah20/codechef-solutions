bdb0ade8ed654d841c2637b32c0a9911b46fd7ca
Rushabh <20rush09abh2001@gmail.com>
2025-12-27 15:58:43 +0530

    Added C++ solution to 'Racing Horses'

---
+++ Solutions/Racing Horses/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Racing Horses
+// Link to the problem: https://www.codechef.com/problems/HORSES
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = a[1] - a[0];
+    for (ll i = 2; i < n; i++)
+    {
+        ans = min(ans, a[i] - a[i - 1]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
