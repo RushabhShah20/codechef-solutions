6f553882f346da95b7c5c50ba33a553410d76b1f
Rushabh <20rush09abh2001@gmail.com>
2025-12-27 16:03:08 +0530

    Added C++ solution to 'Array Reordering'

---
+++ Solutions/Array Reordering/C++/main.cpp
@@ -0,0 +1,46 @@
+// Problem: Array Reordering
+// Link to the problem: https://www.codechef.com/problems/ARRAYREOR
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    sort(a.begin(), a.end(), greater<ll>());
+    sort(b.begin(), b.end());
+    for (ll i = 0; i < n; i++)
+    {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << b[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
