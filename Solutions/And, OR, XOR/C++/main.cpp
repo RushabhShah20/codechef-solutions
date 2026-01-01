a43182c41b34c9d33c3ecc830bec5bb5841ab0e9
Rushabh <20rush09abh2001@gmail.com>
2026-01-01 13:33:58 +0530

    Added C++ solution to 'And, OR, XOR'

---
+++ Solutions/And, OR, XOR/C++/main.cpp
@@ -0,0 +1,19 @@
+// Problem: And, OR, XOR
+// Link to the problem: https://www.codechef.com/problems/BITMEDU1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    cout << (((3 & 5) | 4) ^ 7) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);{
+    solve();
+    return 0;
+}
