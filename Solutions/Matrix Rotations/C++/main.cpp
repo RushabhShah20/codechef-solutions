30e57587160c5c0c29fa3415fdcb4959bed9d10b
Rushabh <20rush09abh2001@gmail.com>
2026-01-01 10:30:01 +0530

    Added C++ solution to 'Matrix Rotations'

---
+++ Solutions/Matrix Rotations/C++/main.cpp
@@ -0,0 +1,24 @@
+// Problem: Matrix Rotations
+// Link to the problem: https://www.codechef.com/problems/MATROTATE
+void rotateClockwise(vector<vector<int>> &matrix)
+{
+    int n = matrix.size();
+    for (int i = 0; i < n / 2; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[n - 1 - i][j];
+            matrix[n - 1 - i][j] = temp;
+        }
+    }
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = i + 1; j < n; j++)
+        {
+            int temp = matrix[i][j];
+            matrix[i][j] = matrix[j][i];
+            matrix[j][i] = temp;
+        }
+    }
+}
