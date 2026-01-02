5f710e98c7f7670e56cdd5af05aefad415c2eb64
Rushabh <20rush09abh2001@gmail.com>
2026-01-02 14:34:36 +0530

    Added C++ solution to 'Devu and friendship testing'

---
+++ Solutions/Devu and friendship testing/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Devu and friendship testing
+// Link to the problem: https://www.codechef.com/problems/CFRTEST
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    set<ll> s;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        s.insert(a[i]);
+    }
+    const ll m = s.size();
+    const ll ans = n - (n - m);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
