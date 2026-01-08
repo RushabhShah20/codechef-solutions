90056ec018e149a8eb2def8046804dbacfb36aaa
Rushabh <20rush09abh2001@gmail.com>
2026-01-08 16:13:42 +0530

    Added C++ solution to 'New Year Resolution'

---
+++ Solutions/New Year Resolution/C++/main.cpp
@@ -0,0 +1,21 @@
+// Problem: New Year Resolution
+// Link to the problem: https://www.codechef.com/problems/NYRES
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    cout << x * 31 << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
