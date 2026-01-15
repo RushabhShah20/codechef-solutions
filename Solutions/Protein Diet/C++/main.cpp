7b2648d7513cadf7886fa4c5ab7836e3e8daa608
Rushabh <20rush09abh2001@gmail.com>
2026-01-15 11:17:11 +0530

    Added C++ solution to 'Protein Diet'

---
+++ Solutions/Protein Diet/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Protein Diet
+// Link to the problem: https://www.codechef.com/problems/LMP1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    if (x >= y)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
