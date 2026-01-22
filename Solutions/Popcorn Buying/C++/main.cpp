035989db553e0690a06e2a1a490b631d29b5a06b
Rushabh <20rush09abh2001@gmail.com>
2026-01-22 12:57:50 +0530

    Added C++ solution to 'Popcorn Buying'

---
+++ Solutions/Popcorn Buying/C++/main.cpp
@@ -0,0 +1,22 @@
+// Problem: Popcorn Buying
+// Link to the problem: https://www.codechef.com/problems/POPCORN7
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n - 100) / 50;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    solve();
+    return 0;
+}
