b2cea79e8ef1d4fc7e91f5d529e609ced478588c
Rushabh <20rush09abh2001@gmail.com>
2026-02-09 11:36:08 +0530

    Added C++ solution to 'Odd Sum'

---
+++ Solutions/Odd Sum/C++/main.cpp
@@ -0,0 +1,28 @@
+// Problem: Odd Sum
+// Link to the problem: https://www.codechef.com/problems/ODDSUM
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    cout << 1 + ((n - 2) * (n - 1)) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
