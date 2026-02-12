0567e41998179d4faab84f51236217144e6df2bb
Rushabh <20rush09abh2001@gmail.com>
2026-02-12 13:32:05 +0530

    Added C++ solution to 'Maximum Score'

---
+++ Solutions/Maximum Score/C++/main.cpp
@@ -0,0 +1,46 @@
+// Problem: Maximum Score
+// Link to the problem: https://www.codechef.com/problems/MXSCWN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+        ans += a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    ll x = LLONG_MAX;
+    for (ll i = 0; i < n; i++)
+    {
+        x = min(x, a[i] - b[i]);
+    }
+    ans -= x;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
