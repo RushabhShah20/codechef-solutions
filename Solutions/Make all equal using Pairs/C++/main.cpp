0d39d2ceadac61b80c558642dca5554658e01a2d
Rushabh <20rush09abh2001@gmail.com>
2026-02-14 15:54:58 +0530

    Added C++ solution to 'Make all equal using Pairs'

---
+++ Solutions/Make all equal using Pairs/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Make all equal using Pairs
+// Link to the problem: https://www.codechef.com/problems/PAIREQ
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    unordered_map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    ll p = 0;
+    for (const pair<ll, ll> i : m)
+    {
+        p = max(p, i.second);
+    }
+    const ll ans = n - p;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
