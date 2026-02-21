09678fcd899729f89a3220f1fe33346a9c2405f8
Rushabh <20rush09abh2001@gmail.com>
2026-02-21 15:14:22 +0530

    Added C++ solution to 'Minimum Number of Pizzas'

---
+++ Solutions/Minimum Number of Pizzas/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Minimum Number of Pizzas
+// Link to the problem: https://www.codechef.com/problems/MINPIZZAS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = n / __gcd(n, k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
