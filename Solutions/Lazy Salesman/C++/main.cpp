3afd3facab7476993bf6071bbe4b3f799e3e803c
Rushabh <20rush09abh2001@gmail.com>
2026-02-26 19:45:46 +0530

    Added C++ solution to 'Lazy Salesman'

---
+++ Solutions/Lazy Salesman/C++/main.cpp
@@ -0,0 +1,48 @@
+// Problem: Lazy Salesman
+// Link to the problem: https://www.codechef.com/problems/HOLIDAYS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, w;
+    cin >> n >> w;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end(), greater<>());
+    ll ans = n, sum = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        if (sum < w)
+        {
+            sum += a[i];
+            ans--;
+        }
+        else
+        {
+            break;
+        }
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
