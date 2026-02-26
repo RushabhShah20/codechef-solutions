d7a880f2a17521fbfc6ae89908bf214647805346
Rushabh <20rush09abh2001@gmail.com>
2026-02-26 16:06:32 +0530

    Added C++ solution to 'Fonta'

---
+++ Solutions/Fonta/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Fonta
+// Link to the problem: https://www.codechef.com/problems/FONTA
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.substr(2, 3) == "nta")
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
