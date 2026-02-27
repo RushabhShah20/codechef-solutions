b3456461dc595b382df6b357da2899f936e73bce
Rushabh <20rush09abh2001@gmail.com>
2026-02-27 15:01:19 +0530

    Added C++ solution to 'Watson asks Does Permutation Exist'

---
+++ Solutions/Watson asks Does Permutation Exist/C++/main.cpp
@@ -0,0 +1,47 @@
+// Problem: Watson asks Does Permutation Exist
+// Link to the problem: https://www.codechef.com/problems/PERMEXIS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll x = 0;
+    for (ll i = 1; i < n; i++)
+    {
+        x = max(x, a[i] - a[i - 1]);
+    }
+    if (x <= 1)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
