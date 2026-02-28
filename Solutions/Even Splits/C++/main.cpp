19a9e9210f3193123ffc9ffc4dcf048d3e38e8e1
Rushabh <20rush09abh2001@gmail.com>
2026-02-28 17:27:57 +0530

    Added C++ solution to 'Even Splits'

---
+++ Solutions/Even Splits/C++/main.cpp
@@ -0,0 +1,39 @@
+// Problem: Even Splits
+// Link to the problem: https://www.codechef.com/problems/EVENSPLIT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    if (n <= 2)
+    {
+        cout << s << endl;
+    }
+    else
+    {
+        sort(s.begin(), s.end());
+        cout << s << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
