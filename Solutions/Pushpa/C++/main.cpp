2b26e395c4135c30bae9f85aa8291525d1e207ac
Rushabh <20rush09abh2001@gmail.com>
2026-02-28 18:28:53 +0530

    Added C++ solution to 'Pushpa'

---
+++ Solutions/Pushpa/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Pushpa
+// Link to the problem: https://www.codechef.com/problems/PUSH7PA
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, ans = 0;
+    cin >> n;
+    map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    for (const pair<ll, ll> i : m)
+    {
+        ans = max(ans, i.first + i.second - 1);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
