8a6a42975b816d7c712b7a859f442a78da8d437b
Rushabh <20rush09abh2001@gmail.com>
2026-02-28 17:01:15 +0530

    Added C++ solution to 'Chef and Linear Chess'

---
+++ Solutions/Chef and Linear Chess/C++/main.cpp
@@ -0,0 +1,52 @@
+// Problem: Chef and Linear Chess
+// Link to the problem: https://www.codechef.com/problems/SUBTASK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        if (k % x == 0)
+        {
+            a.push_back(x);
+        }
+    }
+    const ll m = a.size();
+    if (m == 0)
+    {
+        cout << -1 << endl;
+    }
+    else
+    {
+        ll ans = 0;
+        for (ll i = 0; i < m; i++)
+        {
+            ans = max(ans, a[i]);
+        }
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
