94f0f04ab2fd3c1f07a6d5fa809775707789f11e
Rushabh <20rush09abh2001@gmail.com>
2026-03-01 14:57:52 +0530

    Added C++ solution to 'Make Multiple'

---
+++ Solutions/Make Multiple/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Make Multiple
+// Link to the problem: https://www.codechef.com/problems/MAKEMULTIPLE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    if (2 * a <= b || a == b)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
