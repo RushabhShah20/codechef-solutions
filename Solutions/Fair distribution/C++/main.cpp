716e38a7af5c41db7a3d62b960961a268a99700e
Rushabh <20rush09abh2001@gmail.com>
2026-03-03 13:37:56 +0530

    Added C++ solution to 'Fair distribution'

---
+++ Solutions/Fair distribution/C++/main.cpp
@@ -0,0 +1,40 @@
+// Problem: Fair distribution
+// Link to the problem: https://www.codechef.com/problems/SUBS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    sort(a.begin(), a.end());
+    ll ans = LLONG_MAX;
+    for (ll i = 0; i + k <= n; i++)
+    {
+        ans = min(ans, a[i + k - 1] - a[i]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
