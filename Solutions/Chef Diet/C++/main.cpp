af5ecbd50eb647f3699fdae642f4c65bbe8c661b
Rushabh <20rush09abh2001@gmail.com>
2026-03-04 14:46:30 +0530

    Added C++ solution to 'Chef Diet'

---
+++ Solutions/Chef Diet/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Chef Diet
+// Link to the problem: https://www.codechef.com/problems/DIET
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    ll x = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        x = x + a[i] - k;
+        if (x < 0)
+        {
+            cout << "NO " << i + 1 << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
