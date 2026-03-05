993fce9a9f6db749aa8109b411406d81d33f3b3b
Rushabh <20rush09abh2001@gmail.com>
2026-03-05 11:57:55 +0530

    Added C++ solution to 'Color Festival'

---
+++ Solutions/Color Festival/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Color Festival
+// Link to the problem: https://www.codechef.com/problems/BOP2
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    unordered_set<ll> s;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        s.insert(x);
+    }
+    const ll ans = s.size();
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
