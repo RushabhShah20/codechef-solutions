c418c60da1e32030314a5e1229886da0a6c87661
Rushabh <20rush09abh2001@gmail.com>
2026-03-05 12:00:47 +0530

    Added C++ solution to 'Balloon Smash Party'

---
+++ Solutions/Balloon Smash Party/C++/main.cpp
@@ -0,0 +1,48 @@
+// Problem: Balloon Smash Party
+// Link to the problem: https://www.codechef.com/problems/BOP3E
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    vector<ll> ans(n, 0);
+    ll curr = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ans[i] = min(a[i], curr);
+        if (ans[i] < a[i])
+        {
+            curr++;
+        }
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cout << ans[i] << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
