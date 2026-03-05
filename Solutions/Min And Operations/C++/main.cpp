38d8a3f0b1f51565f6f53a177e64045b7495c501
Rushabh <20rush09abh2001@gmail.com>
2026-03-05 11:58:54 +0530

    Added C++ solution to 'Min And Operations'

---
+++ Solutions/Min And Operations/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Min And Operations
+// Link to the problem: https://www.codechef.com/problems/BOP3
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n / 2) * ((n / 2) + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
