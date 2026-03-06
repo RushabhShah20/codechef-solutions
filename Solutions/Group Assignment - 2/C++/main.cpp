16d91a2c5731d3756607bc7119557b8fd791ec5e
Rushabh <20rush09abh2001@gmail.com>
2026-03-06 11:30:27 +0530

    Added C++ solution to 'Group Assignment'

---
+++ Solutions/Group Assignment - 2/C++/main.cpp
@@ -0,0 +1,44 @@
+// Problem: Group Assignment
+// Link to the problem: https://www.codechef.com/problems/GRPASSN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    for (const pair<ll, ll> i : m)
+    {
+        if (i.second % i.first != 0)
+        {
+            cout << "NO" << endl;
+            return;
+        }
+    }
+    cout << "YES" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
