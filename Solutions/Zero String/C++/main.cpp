709722b5c30826ce8465bd7794bccf2e3aafe7c7
Rushabh <20rush09abh2001@gmail.com>
2026-03-06 16:33:41 +0530

    Added C++ solution to 'Zero String'

---
+++ Solutions/Zero String/C++/main.cpp
@@ -0,0 +1,51 @@
+// Problem: Zero String
+// Link to the problem: https://www.codechef.com/problems/ZEROSTRING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    string s;
+    cin >> s;
+    ll x = 0;
+    for (const char c : s)
+    {
+        if (c == '1')
+        {
+            x++;
+        }
+    }
+    if (x == 0)
+    {
+        cout << 0 << endl;
+    }
+    else if (x == n)
+    {
+        cout << 1 << endl;
+    }
+    else
+    {
+        const ll ans = min(x, n - x + 1);
+        cout << ans << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
