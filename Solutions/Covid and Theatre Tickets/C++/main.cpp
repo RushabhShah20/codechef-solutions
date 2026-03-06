0e99ee90336a8d63aea907380139441f68ba5ae6
Rushabh <20rush09abh2001@gmail.com>
2026-03-06 16:47:09 +0530

    Added C++ solution to 'Covid and Theatre Tickets'

---
+++ Solutions/Covid and Theatre Tickets/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Covid and Theatre Tickets
+// Link to the problem: https://www.codechef.com/problems/COVID_19
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, m;
+    cin >> n >> m;
+    const ll ans = ((n + 1) / 2) * ((m + 1) / 2);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
