2cb30ed4ab44ccacbd5e96ada8bba2f263f56663
Rushabh <20rush09abh2001@gmail.com>
2026-03-07 15:14:41 +0530

    Added C++ solution to 'Football'

---
+++ Solutions/Football/C++/main.cpp
@@ -0,0 +1,43 @@
+// Problem: Football
+// Link to the problem: https://www.codechef.com/problems/MSNSADM1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    vector<ll> a(n), b(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> b[i];
+    }
+    ll ans = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ans = max(ans, 20 * a[i] - 10 * b[i]);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
