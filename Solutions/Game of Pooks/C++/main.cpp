df4e1fe6e6ae49556a3ee05b67143cbb24803628
Rushabh <20rush09abh2001@gmail.com>
2026-03-07 16:50:39 +0530

    Added C++ solution to 'Game of Pooks'

---
+++ Solutions/Game of Pooks/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Game of Pooks
+// Link to the problem: https://www.codechef.com/problems/POOK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n == 2 || n == 3) ? (n - 1) : (n);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
