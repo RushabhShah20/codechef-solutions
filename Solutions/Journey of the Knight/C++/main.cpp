127636df6f38613879a57ade8d6792944379f1cb
Rushabh <20rush09abh2001@gmail.com>
2026-03-07 14:58:18 +0530

    Added C++ solution to 'Journey of the Knight'

---
+++ Solutions/Journey of the Knight/C++/main.cpp
@@ -0,0 +1,36 @@
+// Problem: Journey of the Knight
+// Link to the problem: https://www.codechef.com/problems/KNIGHT2
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x1, y1, x2, y2;
+    cin >> x1 >> y1 >> x2 >> y2;
+    if ((x2 + y2 - x1 - y1) % 2 == 0)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
