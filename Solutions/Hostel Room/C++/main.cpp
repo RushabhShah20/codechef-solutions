84f267d100f4372ef539165127017b3e4d377f92
Rushabh <20rush09abh2001@gmail.com>
2026-03-07 14:10:49 +0530

    Added C++ solution to 'Hostel Room'

---
+++ Solutions/Hostel Room/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Hostel Room
+// Link to the problem: https://www.codechef.com/problems/HOSTELROOM
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    ll ans = x;
+    for (ll i = 0; i < n; i++)
+    {
+        ll y;
+        cin >> y;
+        x += y;
+        ans = max(ans, x);
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
