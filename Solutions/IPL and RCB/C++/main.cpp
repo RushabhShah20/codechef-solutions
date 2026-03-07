ebfb92e4f40147850cc1e8853d6316fdbeb175f9
Rushabh <20rush09abh2001@gmail.com>
2026-03-07 14:06:06 +0530

    Added C++ solution to 'IPL and RCB'

---
+++ Solutions/IPL and RCB/C++/main.cpp
@@ -0,0 +1,29 @@
+// Problem: IPL and RCB
+// Link to the problem: https://www.codechef.com/problems/CLIPLX
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << max(0LL, x - y) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
