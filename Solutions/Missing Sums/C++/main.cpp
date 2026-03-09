3bcb56a4a173706f3fa7762ee090e6ef00f16902
Rushabh <20rush09abh2001@gmail.com>
2026-03-09 15:18:36 +0530

    Added C++ solution to 'Missing Sums'

---
+++ Solutions/Missing Sums/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: Missing Sums
+// Link to the problem: https://www.codechef.com/problems/MISSSUMS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 0; i < n; i++)
+    {
+        cout << 100000 - i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
