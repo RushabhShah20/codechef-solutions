b0bb9bc6eaa7300e106c836029242902ebda463c
Rushabh <20rush09abh2001@gmail.com>
2026-03-12 13:36:53 +0530

    Added C++ solution to 'Vacation Excitement'

---
+++ Solutions/Vacation Excitement/C++/main.cpp
@@ -0,0 +1,24 @@
+// Problem: Vacation Excitement
+// Link to the problem: https://www.codechef.com/problems/VACAEX
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    cout << (y + x - 1) << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
