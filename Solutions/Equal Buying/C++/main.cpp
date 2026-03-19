defaee4834fa2e4b83bb1c78b1e2d1b48080b9d2
Rushabh <20rush09abh2001@gmail.com>
2026-03-19 11:16:55 +0530

    Added C++ solution to 'Equal Buying'

---
+++ Solutions/Equal Buying/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Equal Buying
+// Link to the problem: https://www.codechef.com/problems/EQBUY
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    if (n % 3 == 0)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
