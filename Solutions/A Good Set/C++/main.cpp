1f6c12dc28a800555138145be38b3a757d7b5246
Rushabh <20rush09abh2001@gmail.com>
2026-03-25 14:16:40 +0530

    Added C++ solution to 'A Good Set'

---
+++ Solutions/A Good Set/C++/main.cpp
@@ -0,0 +1,33 @@
+// Problem: A Good Set
+// Link to the problem: https://www.codechef.com/problems/GOODSET
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    for (ll i = 500; i > 500 - n; i--)
+    {
+        cout << i << " ";
+    }
+    cout << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
