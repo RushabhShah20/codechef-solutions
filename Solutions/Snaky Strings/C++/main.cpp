bd24db7f14aac4ae852090c049f1e07191bf027f
Rushabh <20rush09abh2001@gmail.com>
2026-03-26 10:11:55 +0530

    Added C++ solution to 'Snaky Strings'

---
+++ Solutions/Snaky Strings/C++/main.cpp
@@ -0,0 +1,31 @@
+// Problem: Snaky Strings
+// Link to the problem: https://www.codechef.com/problems/SSNK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    if (s.front() == 's' || s.back() == 's')
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
