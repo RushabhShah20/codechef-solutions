460f4303fcf8f2a36c99a52298d6dd2e17adf697
Rushabh <20rush09abh2001@gmail.com>
2026-03-27 19:39:29 +0530

    Added C++ solution to 'Chef and Secret Ingredient'

---
+++ Solutions/Chef and Secret Ingredient/C++/main.cpp
@@ -0,0 +1,42 @@
+// Problem: Chef and Secret Ingredient
+// Link to the problem: https://www.codechef.com/problems/PCJ18A
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, x;
+    cin >> n >> x;
+    vector<ll> a(n);
+    for (ll i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    for (ll i = 0; i < n; i++)
+    {
+        if (a[i] >= x)
+        {
+            cout << "YES" << endl;
+            return;
+        }
+    }
+    cout << "NO" << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
