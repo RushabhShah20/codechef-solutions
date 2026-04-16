5c9de2b4e21007cf9a1ba9e38f288d5d463f30d9
Rushabh <20rush09abh2001@gmail.com>
2026-04-16 14:30:36 +0530

    Added C++ solution to 'Bakery Problem'

---
+++ Solutions/Bakery Problem/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Bakery Problem
+// Link to the problem: https://www.codechef.com/problems/BAKERY7
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll a = 100 * n, b = k + 60 * n, ans = min(a, b);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
