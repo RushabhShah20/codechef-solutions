404f152f834f044b295f9ef5a62a74b4e9a08d8a
Rushabh <20rush09abh2001@gmail.com>
2026-04-30 13:12:13 +0530

    Added C++ solution to 'Extra Guests'

---
+++ Solutions/Extra Guests/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Extra Guests
+// Link to the problem: https://www.codechef.com/problems/EXGS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    const ll ans = x * 100 + (y - x) * 150;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
