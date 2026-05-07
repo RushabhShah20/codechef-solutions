39bcc9e5c820647765a5f96e5da796a33dd5f663
Rushabh <20rush09abh2001@gmail.com>
2026-05-07 13:48:12 +0530

    Added C++ solution to 'Election'

---
+++ Solutions/Election/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Election
+// Link to the problem: https://www.codechef.com/problems/ELECTION1
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = max(0LL, n / 2 + 1 - k);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
