2d28ea2b9542c79771e390dcc62beb331dbadd3e
Rushabh <20rush09abh2001@gmail.com>
2026-05-14 13:05:34 +0530

    Added C++ solution to 'Test Match Target'

---
+++ Solutions/Test Match Target/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Test Match Target
+// Link to the problem: https://www.codechef.com/problems/TESTTGT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    const ll ans = max(0LL, x + z - y + 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
