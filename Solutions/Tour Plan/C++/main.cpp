e8a6169a06265a50a50704aba5f9f82bd36e6646
Rushabh <20rush09abh2001@gmail.com>
2026-05-21 14:11:41 +0530

    Added C++ solution to 'Tour Plan'

---
+++ Solutions/Tour Plan/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Tour Plan
+// Link to the problem: https://www.codechef.com/problems/TOURPLAN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    const ll ans = x + max(0LL, z - 50) * y;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
