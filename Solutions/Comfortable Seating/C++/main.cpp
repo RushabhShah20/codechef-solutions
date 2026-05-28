dd4e29f9f6bf8a2ae3dc2dd113a216ff121b579b
Rushabh <20rush09abh2001@gmail.com>
2026-05-28 15:48:29 +0530

    Added C++ solution to 'Comfortable Seating'

---
+++ Solutions/Comfortable Seating/C++/main.cpp
@@ -0,0 +1,37 @@
+// Problem: Comfortable Seating
+// Link to the problem: https://www.codechef.com/problems/CYCYC
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    map<ll, ll> m;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        m[x]++;
+    }
+    const string ans = m.begin()->second >= 2 ? "YES" : "NO";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
