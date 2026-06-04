bb04435e4425d64ee9ed3c658eb5a579ff0daf04
Rushabh <20rush09abh2001@gmail.com>
2026-06-04 15:25:40 +0530

    Added C++ solution to 'Sum Reduction'

---
+++ Solutions/Sum Reduction/C++/main.cpp
@@ -0,0 +1,38 @@
+// Problem: Sum Reduction
+// Link to the problem: https://www.codechef.com/problems/SUMREDUCTION
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    ll y = 0, z = 0;
+    for (ll i = 0; i < n; i++)
+    {
+        ll x;
+        cin >> x;
+        y += x;
+        z |= x;
+    }
+    const string ans = y == z ? "Yes" : "No";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
