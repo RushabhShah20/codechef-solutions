4165c38984992f668cc55f246e46226037e7d2a7
Rushabh <20rush09abh2001@gmail.com>
2026-06-11 14:12:24 +0530

    Added C++ solution to 'Seated Together'

---
+++ Solutions/Seated Together/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Seated Together
+// Link to the problem: https://www.codechef.com/problems/SAMESIT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const string ans = n % 5 == 0 ? "No" : "Yes";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
