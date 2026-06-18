c39e336dbabde6e3e7eb51343ee026270c7d0ca6
Rushabh <20rush09abh2001@gmail.com>
2026-06-18 14:56:44 +0530

    Added C++ solution to 'Interesting Match'

---
+++ Solutions/Interesting Match/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Interesting Match
+// Link to the problem: https://www.codechef.com/problems/INTMTCH
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y;
+    cin >> x >> y;
+    const string ans = abs(x - y) <= 2 ? "Interesting" : "Boring";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
