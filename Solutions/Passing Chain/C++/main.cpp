3f4cf254a1605abadbf718e41bfc765d541af5af
Rushabh <20rush09abh2001@gmail.com>
2026-06-27 15:41:55 +0530

    Added C++ solution to 'Passing Chain'

---
+++ Solutions/Passing Chain/C++/main.cpp
@@ -0,0 +1,34 @@
+// Problem: Passing Chain
+// Link to the problem: https://www.codechef.com/problems/PASSCHAIN
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    ll ans = 1;
+    while (ans + k <= n)
+    {
+        ans += k;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
