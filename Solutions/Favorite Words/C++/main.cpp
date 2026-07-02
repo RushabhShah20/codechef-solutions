13bcea0eb63c40ef2f3af4e8810d6ddde09ee953
Rushabh <20rush09abh2001@gmail.com>
2026-07-02 16:07:49 +0530

    Added C++ solution to 'Favorite Words'

---
+++ Solutions/Favorite Words/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Favorite Words
+// Link to the problem: https://www.codechef.com/problems/FAVWD
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const string ans = s[0] == 'c' || s[3] == 'f' ? "Yes" : "No";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
