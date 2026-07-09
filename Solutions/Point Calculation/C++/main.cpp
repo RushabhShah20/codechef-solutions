f9bd9731cc1c777baacaa37d6a917d88fd8dd5e0
Rushabh <20rush09abh2001@gmail.com>
2026-07-09 15:22:04 +0530

    Added C++ solution to 'Point Calculation'

---
+++ Solutions/Point Calculation/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Point Calculation
+// Link to the problem: https://www.codechef.com/problems/POINTCAL
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b, c;
+    cin >> a >> b >> c;
+    const ll ans = 3 * a + b;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
