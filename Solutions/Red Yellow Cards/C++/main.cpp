b24070b9b72be9333e08e6a21006323b39db2516
Rushabh <20rush09abh2001@gmail.com>
2026-07-12 08:27:37 +0530

    Added C++ solution to 'Red Yellow Cards'

---
+++ Solutions/Red Yellow Cards/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Red Yellow Cards
+// Link to the problem: https://www.codechef.com/problems/RYCARDS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll ans = a + max(0LL, b - a) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
