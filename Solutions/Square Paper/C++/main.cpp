41a6a79ee832c5f6a5c1e19fb151abbb2fe9e4e5
Rushabh <20rush09abh2001@gmail.com>
2026-07-16 15:29:54 +0530

    Added C++ solution to 'Square Paper'

---
+++ Solutions/Square Paper/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Square Paper
+// Link to the problem: https://www.codechef.com/problems/SQPAPER
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll x = min(a, b), ans = x * x;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
