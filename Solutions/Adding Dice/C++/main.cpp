54cd90f60adc7a921bc2f2766e3c64fb0317c7f4
Rushabh <20rush09abh2001@gmail.com>
2026-07-29 17:00:16 +0530

    Added C++ solution to 'Adding Dice'

---
+++ Solutions/Adding Dice/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Adding Dice
+// Link to the problem: https://www.codechef.com/problems/ADDDICE
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const string ans = n >= 3 ? "Yes" : "No";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
