e2c7fdc5dc26411a4934c816cd44815c0aba6ae1
Rushabh <20rush09abh2001@gmail.com>
2026-08-01 07:32:54 +0530

    Added C++ solution to 'Product Exceed'

---
+++ Solutions/Product Exceed/C++/main.cpp
@@ -0,0 +1,35 @@
+// Problem: Product Exceed
+// Link to the problem: https://www.codechef.com/problems/PRDEXC
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll x, y, z;
+    cin >> x >> y >> z;
+    ll ans = 0;
+    while (x * y < z)
+    {
+        x >= y ? y++ : x++;
+        ans++;
+    }
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
