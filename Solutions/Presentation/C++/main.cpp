a85592f78ccb4bf271d0443fe47411fe76dbd705
Rushabh <20rush09abh2001@gmail.com>
2026-08-06 11:50:50 +0530

    Added C++ solution to 'Presentation'

---
+++ Solutions/Presentation/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Presentation
+// Link to the problem: https://www.codechef.com/problems/PPT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = 20 - n;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
