31cd985f416a0fb94967f201f28471ac87047802
Rushabh <20rush09abh2001@gmail.com>
2026-09-05 11:00:12 +0530

    Added C++ solution to 'Reach Weight'

---
+++ Solutions/Reach Weight/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Reach Weight
+// Link to the problem: https://www.codechef.com/problems/REACHWT
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n;
+    cin >> n;
+    const ll ans = (n >> 1) * 30 + (n & 1) * 20;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
