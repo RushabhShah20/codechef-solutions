037fcb70ec4b1fdbe8f6620275ddd56d8434be1b
Rushabh <20rush09abh2001@gmail.com>
2026-09-17 10:50:34 +0530

    Added C++ solution to 'Sharing Cookies'

---
+++ Solutions/Sharing Cookies/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Sharing Cookies
+// Link to the problem: https://www.codechef.com/problems/SHARING
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll a, b;
+    cin >> a >> b;
+    const ll ans = (a - b) & 1 ? -1 : (a - b) / 2;
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
