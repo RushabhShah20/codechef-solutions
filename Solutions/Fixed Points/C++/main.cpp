e7a2b9c44b4c400f497953f31afedadbd321731b
Rushabh <20rush09abh2001@gmail.com>
2026-09-19 11:15:02 +0530

    Added C++ solution to 'Fixed Points'

---
+++ Solutions/Fixed Points/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Fixed Points
+// Link to the problem: https://www.codechef.com/problems/FIXEDPTS
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const string ans = n - 1 == k ? "No" : "Yes";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
