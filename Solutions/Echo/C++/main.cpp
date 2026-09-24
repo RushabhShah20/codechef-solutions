81ab111e0562d48a4efbea54f48520710146bc91
Rushabh <20rush09abh2001@gmail.com>
2026-09-24 10:26:02 +0530

    Added C++ solution to 'Echo'

---
+++ Solutions/Echo/C++/main.cpp
@@ -0,0 +1,25 @@
+// Problem: Echo
+// Link to the problem: https://www.codechef.com/problems/ECHOECHO
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    string s;
+    cin >> s;
+    const string ans = s[0] == s[2] && s[1] == s[3] ? "Yes" : "No";
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    solve();
+    return 0;
+}
