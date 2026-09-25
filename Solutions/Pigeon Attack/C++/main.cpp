734259f995440beaa16724dfc2e1c3de830d9660
Rushabh <20rush09abh2001@gmail.com>
2026-09-25 12:24:23 +0530

    Added C++ solution to 'Pigeon Attack'

---
+++ Solutions/Pigeon Attack/C++/main.cpp
@@ -0,0 +1,30 @@
+// Problem: Pigeon Attack
+// Link to the problem: https://www.codechef.com/problems/PGNATK
+#include <bits/stdc++.h>
+#define ll long long int
+#define ull unsigned long long int
+using namespace std;
+
+void solve()
+{
+    ll n, k;
+    cin >> n >> k;
+    const ll ans = n + (n - 1) / (k - 1);
+    cout << ans << endl;
+}
+
+int main()
+{
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    cout.tie(nullptr);
+    ll t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
